219847b55ad0a1e92da5cf690e05727f40d45f7a
williamwordsworth <86642294+williamwordsworth@users.noreply.github.com>
2021-08-11 22:20:58 +0600

    Update NGROK-Setup.bat

--- NGROK-Setup.bat
+++ NGROK-Setup.bat
@@ -47,10 +47,10 @@ echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-r
 sh ip.sh
 echo User: runneradmin
 echo Pass: %_Password%
-curl -O https://raw.githubusercontent.com/mrijoo/RDP/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/mrijoo/RDP/main/Files/FastConfigVPS_v5.1.exe > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\Everything.exe" https://raw.githubusercontent.com/mrijoo/RDP/main/Files/Everything.exe > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\Windows-User.bat" https://raw.githubusercontent.com/mrijoo/RDP/main/Files/Windows-User.bat > out.txt 2>&1
+curl -O https://raw.githubusercontent.com/williamwordsworth/RDP-2/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/williamwordsworth/RDP-2/main/Files/FastConfigVPS_v5.1.exe > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Everything.exe" https://raw.githubusercontent.com/williamwordsworth/RDP-2/main/Files/Everything.exe > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Windows-User.bat" https://raw.githubusercontent.com/williamwordsworth/RDP-2/main/Files/Windows-User.bat > out.txt 2>&1
 
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
 diskperf -Y >nul
